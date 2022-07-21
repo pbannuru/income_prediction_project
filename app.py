c28315d526ab5c7aa398cbf56cc40c6c08468dfa
pbannuru <pbannuru@gmail.com>
2022-07-21 16:39:33 +0530

    updated

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET','POST'])
+def index():
+    return "start of income prediction project"
+
+
+
+if __name__ = "__main__":
+    app.run(debug=True)
+
+    
